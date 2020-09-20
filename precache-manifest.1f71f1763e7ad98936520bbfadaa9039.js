ad12d5b6ec2687c8a15762adbb18ab683a9ec6f8
matthew29tang <matthew29tang@gmail.com>
2020-09-20 08:51:49 -0700

    Updates

---
+++ precache-manifest.1f71f1763e7ad98936520bbfadaa9039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af187a6132b00fc6664afb82f2925084",
+    "url": "/covid-track/index.html"
+  },
+  {
+    "revision": "ce9332a694a0cfd1bd85",
+    "url": "/covid-track/static/css/main.0257887c.chunk.css"
+  },
+  {
+    "revision": "7b9fa61c01a416a3ae14",
+    "url": "/covid-track/static/js/2.003c3be4.chunk.js"
+  },
+  {
+    "revision": "6cd7422b4a94aef5d7b0939c9d09d3ff",
+    "url": "/covid-track/static/js/2.003c3be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9332a694a0cfd1bd85",
+    "url": "/covid-track/static/js/main.75bf604a.chunk.js"
+  },
+  {
+    "revision": "7301e6c8983484699b3d",
+    "url": "/covid-track/static/js/runtime-main.b6fb3b24.js"
+  }
+]);
